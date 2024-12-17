eb735edc33338be054764c4e33b7fdbcf60605f6
Film_ <pawich23013@gmail.com_>
2024-12-17 15:52:27 +0700

    lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,9 +6,14 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
+	string textline;
+	cout << "-------------------- BOOM ---------------------" << endl;
+	while(getline(source,textline)){
+	    cout << textline << endl;
+	}
+	cout << "-------------------- HA!! ---------------------";
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
